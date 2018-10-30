aee24af27c78667048950186b424bacea77bd50d
tomopool <subadd.mi@gmail.com>
2018-10-31 01:35:13 +0900

    iroiro

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,67 +1,19 @@
 <template>
   <div>
-    <h1>やることリスト</h1>
-    <el-table
-      :data="declarations"
-      height="250"
-      style="width: 100%">
-      <el-table-column
-        prop="declaration"
-        label="declaration">
-      </el-table-column>
-      <el-table-column
-        prop="create_date"
-        label="create_date">
-      </el-table-column>
-      <el-table-column
-        prop="after_time"
-        label="after_time">
-      </el-table-column>
-      <el-table-column
-        prop="specified_time"
-        label="specified_time">
-      </el-table-column>
-    </el-table>
-    <div>
-      <button @click="toMain">もどる</button>
-    </div>
+    <table-view v-if="viewMode === 'table'" />
+    <card-view v-if="viewMode === 'card'" />
   </div>
 </template>
 
-
 <script>
-import {
-  startListener
-} from '../store/actions.js'
+import TableView from './Table'
+import CardView from './Card'
 
 export default {
-  mounted () {
-    this.start()
-  },
-  destroyed () {
-    this.$store.dispatch('clearDeclarations')
-  },
-  methods: {
-    // init () {
-    //   this.$store.dispatch('memos/clear')
-    // },
-    start () {
-      // this.$store.dispatch('startListener')
-      this.$store.dispatch('getDeclarations')
-    },
-    // stop () {
-    //   this.$store.dispatch('memos/stopListener')
-    // },
-    // remove (id) {
-    //   this.$store.dispatch('clearDeclarations')
-    // },
-    toMain() {
-      this.$router.push({name: 'Main'})
-    }
-  },
-  computed: {
-    declarations () {
-      return this.$store.state.declarations
+  components: {TableView, CardView},
+  data() {
+    return {
+      viewMode: 'card'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,16 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import { Input, TimePicker, InputNumber, Table, TableColumn } from 'element-ui'
+import { Input, Button, TimePicker, InputNumber, Table, TableColumn, Card } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Input)
+Vue.use(Button)
 Vue.use(TimePicker)
 Vue.use(InputNumber)
 Vue.use(Table)
 Vue.use(TableColumn)
+Vue.use(Card)
 
 /* eslint-disable no-new */
 new Vue({
