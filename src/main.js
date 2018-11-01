73d0ed94cc8aaf79b2db08fc4c0a4a98f112a975
ikezoet <ikezoet@intra-mart.jp>
2018-11-01 09:56:31 +0900

    main

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Input, TimePicker, InputNumber } from 'element-ui'
+import { Input, TimePicker, InputNumber, Button, Collapse, CollapseItem, Message } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
@@ -11,6 +11,11 @@ Vue.config.productionTip = false
 Vue.use(Input)
 Vue.use(TimePicker)
 Vue.use(InputNumber)
+Vue.use(Button)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+
+Vue.prototype.$message = Message;
 
 /* eslint-disable no-new */
 new Vue({
