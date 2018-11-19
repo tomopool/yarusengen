6faa6fbdf7057dfc6b5df2c7361d69fd2a803128
tomopool <subadd.mi@gmail.com>
2018-11-19 23:28:07 +0900

    テーブル

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="table-view-outer">
     <el-table
-      :data="declarations"
+      :data="tableData"
       height="250"
       style="width: 100%">
       <el-table-column
         prop="declaration"
-        label="declaration">
+        label="宣言">
       </el-table-column>
       <el-table-column
         prop="create_date"
-        label="create_date">
+        label="宣言日">
       </el-table-column>
       <el-table-column
-        prop="after_time"
-        label="after_time">
-      </el-table-column>
-      <el-table-column
-        prop="specified_time"
-        label="specified_time">
+        prop="when_do_it"
+        label="いつやる？">
       </el-table-column>
     </el-table>
   </div>
@@ -32,6 +28,18 @@ export default {
     declarations: {
       type: Array,
       default: () => []
+    }
+  },
+  computed: {
+    tableData() {
+      const data = this.declarations.map(declarationInfo => {
+        return {
+          declaration: declarationInfo.declaration,
+          create_date: this.formatDate(declarationInfo.create_date),
+          when_do_it: this.whenDoIt(declarationInfo)
+        }
+      })
+      return data
     }
   },
   methods: {
